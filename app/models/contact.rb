51141748f3b6d44e072aaef66a77bf8d15c04d3d
Kevin Short <kevinshort88@gmail.com>
2021-09-20 13:09:58 -0600

    models

---
+++ app/models/contact.rb
@@ -0,0 +1,49 @@
+class Contact < ApplicationRecord
+  # Associations - describing relations ships 
+# belongs to child models , who the partents is 
+# - both ways
+# # parents models
+# has_one - related to a single record
+# has_many - related to multiple records
+# has_man_ :through adv
+#     dependent - this will help when we delete the parent it will delete all of its children 
+  
+  # Validations
+
+  # Model Methods
+
+  has_many :notes, dependent: :destroy
+  has_one :address, dependent: :destroy
+
+  # - model validations, stop in the model to validate the data going into the db/
+
+  # most commonx
+  # validating the attr , feilds fo the table / model 
+
+  # confirmation  
+  # - two fields same content
+
+  # inclusion
+  #  - makes sure its one fo the items you give it
+
+  # length
+  # class Person < ActiveRecord::base
+  #   validates: name, length { minimum , 2}
+  # - chatecter count of the value
+
+  # numericality
+  # <clase Playser < ActiveRecord::base
+  # validates : points< numericality: true
+  #  - numeric value
+
+  # # presence
+  # class Person < AciveRecord::base
+  #   validates :name, :login, :email, presence: true
+  #  - makes sure that its not empty or nul
+  # uniquneess
+  #  - one object that gets save of the value 
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :ages, numericality: true
+  validates :email, email: uniquneess: true
+end
